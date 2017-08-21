c334308b9fed2ce2ab45a778dadf22168ca475e2
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-08-21 08:58:00 -0700

    Configure webpack

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
